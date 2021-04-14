e376a96b81e48f830cf7a6ae743d5fdb21a9d263
HugeBrain16 <joshtuck373@gmail.com>
2021-04-13 17:43:35 -0700

    add tests

---
+++ tests/test_parse.py
@@ -0,0 +1,19 @@
+import unittest
+from .. import cmdtools
+
+class TestParse(unittest.TestCase):
+	def test_parse(self):
+		cmd = '/say "hello world"'
+		cmd = cmdtools.Cmd(cmd)
+		cmd.parse()
+
+		self.assertEqual('say', cmd.name, 'boo')
+		self.assertIn('hello world', cmd.args, 'boo')
+
+	def test_parse_eval(self):
+		cmd = '/sum 504 100.0 .54 10.'
+		cmd = cmdtools.Cmd(cmd)
+		cmd.parse(eval=True)
+
+		self.assertIsInstance(cmd.args[0], int, 'boo')
+		self.assertIsInstance(sum(cmd.args[1:]), float, 'boo')
---
+++ tests/test_process.py
@@ -0,0 +1,33 @@
+import unittest
+from .. import cmdtools
+
+class TestProcess(unittest.TestCase):
+	def test_process(self):
+		cmd = '/sum 10 10 10'
+		cmd = cmdtools.Cmd(cmd)
+		cmd.parse(eval=True)
+
+		res = cmdtools.ProcessCmd(cmd, TestProcess.sum)
+
+		self.assertEqual(res, 30, 'boo')
+
+	def test_match(self):
+		cmd = '/sell "Gold" 50'
+		cmd = cmdtools.Cmd(cmd)
+		cmd.parse(eval=True)
+
+		match = cmdtools.MatchArgs(cmd, 'si')
+
+		self.assertTrue(match, 'boo')
+
+	def test_match_(self):
+		cmd = '/add 10 40 20 59'
+		cmd = cmdtools.Cmd(cmd)
+		cmd.parse(eval=True)
+
+		match = cmdtools.MatchArgs(cmd, ('i'*cmd.args_count))
+
+		self.assertTrue(match, 'boo')
+
+	def sum(raw_args, args):
+		return sum(args)
