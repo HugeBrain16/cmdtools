0c5891bf8c2641c31532e0d7ec16445c16833cf2
HugeBrain16 <joshtuck373@gmail.com>
2021-07-25 02:01:13 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ from cmdtools import __version__ as version
 
 setuptools.setup(
     name="cmdtools-py",
-    description="a module for parsing and processing commands.",
+    description="command text parser and command processor",
     version=version,
     author="HugeBrain16",
     author_email="joshtuck373@gmail.com",
     license="MIT",
     keywords="command-parser command-processor command cmd cmd-parser",
     url="https://github.com/HugeBrain16/cmdtools",
-    py_modules=["cmdtools"],
+    packages=["cmdtools", "cmdtools/ext"],
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
     classifiers=[
