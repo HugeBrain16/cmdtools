1d7b1648894828513654b5ad98780db6074e22c0
HugeBrain16 <joshtuck373@gmail.com>
2021-07-27 00:15:50 +0200

    add `test_attributes`

---
+++ tests/test_attributes.py
@@ -0,0 +1,11 @@
+from .. import cmdtools
+
+def _test_func():
+	assert _test_func.test == 20
+
+def test_def_attributes():
+	cmd = cmdtools.Cmd("/test")
+	setattr(_test_func, "test", 40)
+	cmd.process_cmd(_test_func, attrs={"test": 20})
+
+	assert _test_func.test == 40
