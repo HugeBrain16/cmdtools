0968f722f6a24a687599c507d6dae9b2a961ae62
HugeBrain16 <joshtuck373@gmail.com>
2021-08-16 07:51:16 +0200

    update extension test

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -2,23 +2,43 @@ import asyncio
 from .. import cmdtools
 from ..cmdtools.ext import command
 
-class Add(command.Command):
-	def __init__(self):
-		self.result = None
-		self.name = "add"
-		super().__init__(name=self.name)
 
-	def add(self, n1, n2):
-		self.result = n1 + n2
+class Add(command.Command):
+    def __init__(self):
+        self.result = None
+        self.name = "add"
+        super().__init__(name=self.name)
+
+    def add(self, n1, n2):
+        self.result = n1 + n2
+
 
 async def _test_class_cmd():
-	runner = command.CommandRunner(Add())
-	await runner.run(
-		cmdtools.Cmd("/add 40 40", convert_args=True)
-	)
+    runner = command.CommandRunner(Add())
+    await runner.run(
+        cmdtools.Cmd("/add 40 40", convert_args=True)
+    )
 
-	assert runner.command.result == 80
-	assert runner.command.name == "add"
+    assert runner.command.result == 80
+    assert runner.command.name == "add"
+
 
 def test_class_cmd_run():
-	asyncio.run(_test_class_cmd())
+    asyncio.run(_test_class_cmd())
+
+
+wrapper = command.CommandWrapper()
+
+
+@wrapper.command(name='add', aliases=['plus'])
+def add(n, y):
+    return n + y
+
+
+def test_command_wrapper():
+    result = asyncio.run(
+        wrapper.run(cmdtools.Cmd('/add 10 10', convert_args=True))
+    )
+
+    assert result == 20
+    assert 'plus' in add.aliases
