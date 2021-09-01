6c706c192639fefd9a92f2cbd52f961e6a606524
HugeBrain16 <joshtuck373@gmail.com>
2021-09-01 04:34:31 +0200

    update examples

--- examples/04asynchronous_callbacks/main.py
+++ examples/04asynchronous_callbacks/main.py
@@ -2,18 +2,19 @@ import cmdtools
 import inspect
 import asyncio
 
+
 # error handler callback needs to be a coroutine as well as the command callback
 async def eping(error):
-	print(error)
+    print(error)
+
 
 async def ping():
-	print("Pinging...")
-	await asyncio.sleep(1)
-	print("Pong!")
+    print("Pinging...")
+    await asyncio.sleep(1)
+    print("Pong!")
 
-cmd = cmdtools.Cmd("/ping")
+# use asyncio instance Cmd for processing coroutine callbacks
+cmd = cmdtools.AioCmd("/ping")
 
 if cmd.name == "ping":
-	# check if callback is a coroutine function
-	if inspect.iscoroutinefunction(ping):
-		asyncio.run(cmd.aio_process_cmd(ping))
+    asyncio.run(cmd.process_cmd(ping))
