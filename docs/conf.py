d632d92be00c6442b541e11a3748f224592b0e50
HugeBrain16 <joshtuck373@gmail.com>
2021-10-27 04:40:03 +0200

    remove source suffix

--- docs/conf.py
+++ docs/conf.py
@@ -34,12 +34,6 @@ extensions = [
     "sphinx.ext.autodoc",
 ]
 
-source_suffix = {
-    ".md": "markdown",
-    ".rst": "restructuredtext",
-    ".txt": "markdown"
-}
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
