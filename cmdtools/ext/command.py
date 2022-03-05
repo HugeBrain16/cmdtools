5b61dd4f25940cd0dda48ee59e3593838fa4d7ad
HugeBrain16 <joshtuck373@gmail.com>
2022-03-06 02:56:40 +0900

    wraps Callback object when assigning to container

--- cmdtools/ext/command.py
+++ cmdtools/ext/command.py
@@ -114,8 +114,16 @@ class Group(Container):
             if inspect.isclass(obj) and Command in inspect.getmro(obj):
                 self.commands.append(obj())
             else:
-                wrapper = GroupWrapper(name or obj.__name__, aliases)
-                wrapper._callback = Callback(obj)
+                if isinstance(obj, Callback):
+                    name = obj.func.__name__
+                elif isinstance(obj, Callable):
+                    name = obj.__name__
+
+                wrapper = GroupWrapper(name, aliases)
+                if isinstance(obj, Callback):
+                    wrapper._callback = obj
+                else:
+                    wrapper._callback = Callback(obj)
                 self.commands.append(wrapper)
 
                 return wrapper
