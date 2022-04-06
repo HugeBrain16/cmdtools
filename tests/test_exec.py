708ebc1b4ada93e9ee01ac261222782cbf244884
HugeBrain16 <joshtuck373@gmail.com>
2022-04-07 03:02:41 +0900

    rename test callback

--- tests/test_exec.py
+++ tests/test_exec.py
@@ -10,7 +10,7 @@ def sumcmd(ctx: cmdtools.callback.Context):
 
 
 @cmdtools.callback.add_option("num", default=1, type=int)
-def test(ctx: cmdtools.callback.Context):
+def retnum(ctx: cmdtools.callback.Context):
     return ctx.options.num
 
 
@@ -23,10 +23,10 @@ class TestExec(unittest.TestCase):
         self.assertEqual(res, 30)
 
     def test_options(self):
-        cmd1 = cmdtools.Cmd("/test 2")
-        cmd2 = cmdtools.Cmd("/test")
-        exc1 = cmdtools.Executor(cmd1, test)
-        exc2 = cmdtools.Executor(cmd2, test)
+        cmd1 = cmdtools.Cmd("/retnum 2")
+        cmd2 = cmdtools.Cmd("/retnum")
+        exc1 = cmdtools.Executor(cmd1, retnum)
+        exc2 = cmdtools.Executor(cmd2, retnum)
 
         self.assertEqual(exc1.exec(), 2)
         self.assertEqual(exc2.exec(), 1)
