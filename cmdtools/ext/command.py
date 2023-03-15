b871201a324aac3dc1f0d9da6df85ae161ef85ba
HugeBrain16 <hugebrain16@gmail.com>
2023-03-16 06:20:53 +0700

    Revert to old naming approach

--- cmdtools/ext/command.py
+++ cmdtools/ext/command.py
@@ -21,26 +21,17 @@ class BaseCommand:
     @property
     def callback(self) -> Optional[Callback]:
         """Returns the callback of the command object if set."""
-        # Check if the command object has a callback
         if isinstance(self._callback, Callback):
             return self._callback
         else:
-            # Get the name of the command function and the error handler function
-            command_function_name = self.name
-            error_handler_function_name = "error_" + self.name
+            cmdfun = getattr(self, self.name, None)
+            errfun = getattr(self, "error_" + self.name, None)
 
-            # Use the getattr function to retrieve the command function and error handler function
-            command_function = getattr(self, command_function_name, None)
-            error_handler_function = getattr(self, error_handler_function_name, None)
+            if inspect.ismethod(cmdfun):
+                self._callback = Callback(cmdfun)
 
-            # Check if the command function is a method
-            if inspect.ismethod(command_function):
-                # Create a Callback object for the command function
-                self._callback = Callback(command_function)
-
-                # If an error handler function is defined, create an ErrorCallback object for it
-                if error_handler_function:
-                    self._callback.errcall = ErrorCallback(error_handler_function)
+                if errfun:
+                    self._callback.errcall = ErrorCallback(errfun)
 
                 return self._callback
 
