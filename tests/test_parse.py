67ffdef7014aa99a089a8f2d65551f50881f6570
HugeBrain16 <hugebrain16@gmail.com>
2025-04-06 21:54:31 +0700

    add test for parameter argument

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,6 +1,9 @@
 import unittest
 import cmdtools
+from cmdtools import Cmd
+from cmdtools.callback import Options, Option
 from cmdtools.utils.string import PrefixChecker
+from cmdtools.ext.param import apply_params
 
 
 class TestParse(unittest.TestCase):
@@ -19,3 +22,31 @@ class TestParse(unittest.TestCase):
         self.assertEqual(cmd0.strip_prefix, "ping")
         self.assertEqual(cmd1.strip_prefix, "ping")
         self.assertEqual(cmd2.strip_prefix, "ping")
+
+    def test_parse_param(self):
+        cmd = Cmd('/spawn 101 x=1.0 z=1.0')
+        options = Options([
+            Option("id"),
+            Option("x", default="0.0"),
+            Option("y", default="0.0"),
+            Option("z", default="0.0")
+        ])
+
+        apply_params(cmd, options)
+
+        self.assertEqual(options.id, "101")
+        self.assertEqual(options.x, "1.0")
+        self.assertEqual(options.y, None)
+        self.assertEqual(options.z, "1.0")
+
+    def test_parse_param_with_quotes(self):
+        cmd = Cmd('/query user="asdf123" quote=\'"4" is my favorite number\'')
+        options = Options([
+            Option("user"),
+            Option("quote")
+        ])
+
+        apply_params(cmd, options)
+
+        self.assertEqual(options.user, "asdf123")
+        self.assertEqual(options.quote, '"4" is my favorite number')
